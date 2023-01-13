e6b71f645417ee7647b3e14857594166e0d25d98
pinnaree <pprommitr@gmail.com>
2023-01-13 16:09:21 +0700

    lab14-1 compp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,66 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+double AM(const double x[], int n){
+    double sum = 0;
+    double am;
+    for(int i=0; i<n; i++){
+        sum += x[i];
+    }
+    am= sum/n;
+    return am;
+
+}
+
+double SD(const double x[], int n){
+    double sum=0;
+    double sd;
+    for(int i=0; i<n; i++){
+        sum += pow(x[i],2);
+    }
+        sd = sqrt((sum/n)-(pow(AM(x,n),2)) );
+        return  sd;
+}
+
+double GM(const double x[], int n){
+    double sum=0;
+    for(int i=0; i<n; i++){
+    sum += log(x[i]);
+}
+    return exp(sum/n);
+}
+double HM(const double x[],int n){
+     double sum=0;
+    for(int i=0; i<n; i++){
+    sum += 1/x[i];
+}
+    return n/sum;
+
+}
+
+double Max(const double x[],int n){
+     double max= x[0];
+    for(int i=0; i<n; i++){
+    if(x[i]>max) max=x[i];
+}
+    return max;
+
+}
+double Min(const double x[],int n){
+     double min= x[0];
+    for(int i=0; i<n; i++){
+    if(x[i]<min) min=x[i];
+}
+    return min;
+
+}
+void stat(const double A[],int n ,double B[]){
+    B[0]= AM(A, n);
+    B[1]= SD(A, n);
+    B[2]= GM(A, n);
+    B[3]= HM(A, n);
+    B[4]= Max(A, n);
+    B[5]= Min(A, n);
+
+}
